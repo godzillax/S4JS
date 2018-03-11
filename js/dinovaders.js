2705f8eae26e29c3809dec637972162e8741bea9
Unknown <laurancy.dorian@gmail.com>
2018-03-11 16:00:59 +0100

    Gestion des FPS dans le main

--- js/dinovaders.js
+++ js/dinovaders.js
@@ -11,38 +11,64 @@
 function initGame() {
     player = new Player();
     this.player.init();
-    go();
-}
 
-/**
- * Launches the game
- */
-function go() {
     document.body.addEventListener("keydown", playerAction);
-    this.mainLoop();
+    lastPressedKey = 0;
+
+    fps = 60;
+    lastTimeStampUpdate = 0;
+    
+    requestAnimationFrame(mainLoop);
 }
 
 /**
- * Manages the player Actions
+ * Record the last key pressed by the player to makt its action
  * @param {type} event
  */
-function playerAction (event) {
-    var k = event.keyCode;
+function playerAction(event) {
+    lastPressedKey = event.keyCode;
+}
+
+/**
+ * Returns the running time of the application
+ * @returns {Number}
+ */
+function timestamp() {
+    return window.performance && window.performance.now ? window.performance.now() : new Date().getTime();
+}
+/**
+ * Manages the player Actions
+ */
+function makePlayerAction() {
     // Up or Z
-    if (k == 38 || k == 90) 
+    if (lastPressedKey == 38 || lastPressedKey == 90)
         player.up();
     // Down or S 
-    else if (k == 40 || k == 83) 
+    else if (lastPressedKey == 40 || lastPressedKey == 83)
         player.down();
     // Space
-    else if (k == 32)
+    else if (lastPressedKey == 32)
         player.shoot();
+    lastPressedKey = 0;
+}
+
+/**
+ * Update the game, player and enemy position
+ */
+function update() {
+    makePlayerAction()
 }
 
 /**
  * Main Loop of the Game
+ * Calls the update method 'fps' times a second
  */
 function mainLoop() {
-
+    t = timestamp()
+    if (t > lastTimeStampUpdate + (1000 / fps)) {
+        update();
+        lastTimeStampUpdate = t;
+    }
+    requestAnimationFrame(mainLoop);
 }
 
