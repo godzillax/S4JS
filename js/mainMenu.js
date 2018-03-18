8ff452359aba5c6f413fd3df7186d197c9525af0
Unknown <laurancy.dorian@gmail.com>
2018-03-18 21:46:15 +0100

    Main Menu

--- js/mainMenu.js
+++ js/mainMenu.js
@@ -1,3 +1,28 @@
 function initMainMenu() {
-    initGame();
+    mainMenuDiv = document.createElement("div")
+    mainMenuDiv.id = "mainMenu"
+    mainMenuDiv.style.position = "fixed"
+    
+    gameTitle = document.createElement("img")
+    gameTitle.src = "./Images/LogoPlanete.png"
+    
+    mainMenuStartButton = document.createElement("img")
+    mainMenuStartButton.src = "./Images/start.png"
+    mainMenuStartButton.addEventListener("click", launch);
+    mainMenuStartButton.className = "mainMenuButton"
+    
+    displayMainMenu()
+}
+
+function displayMainMenu() {
+    document.getElementById("playground").appendChild(mainMenuDiv)
+    mainMenuDiv.appendChild(gameTitle)
+    mainMenuDiv.appendChild(mainMenuStartButton)
+    
+}
+
+function launch() {
+    mainMenuDiv.innerHTML = "";
+    mainMenuDiv.parentNode.removeChild(mainMenuDiv)
+    initGame()
 }
